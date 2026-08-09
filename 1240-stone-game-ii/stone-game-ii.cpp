fae8584f0a0b11e4781c3b502e93ece0eb28416f
Anirban Bhattacharyya <100213993+Anirban710@users.noreply.github.com>
2026-08-09 10:55:50 +0530

    Time: 11 ms (54.67%) | Memory: 13.5 MB (64.48%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -2,21 +2,28 @@ class Solution {
 public:
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<int> suf(n + 1, 0);
-        for (int i = n - 1; i >= 0; i--)
-            suf[i] = suf[i + 1] + piles[i];
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        function<int(int, int)> solve = [&](int i, int m) {
-            if (i == n)
-                return 0;
-            if (dp[i][m] != -1)
-                return dp[i][m];
-            int ans = 0;
-            for (int x = 1; x <= 2 * m && i + x <= n; x++)
-                ans = max(ans, suf[i] - solve(i + x, max(m, x)));
-            return dp[i][m] = ans;
-        };
-
-        return solve(0, 1);
+        vector<vector<int>> dp(n, vector<int>(n + 1, 0));
+        vector<int> suffixSum(n, 0);
+        
+        suffixSum[n - 1] = piles[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + piles[i];
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int m = 1; m <= n; m++) {
+                if (i + 2 * m >= n) {
+                    dp[i][m] = suffixSum[i];
+                } else {
+                    int minOpponentScore = INT_MAX;
+                    for (int x = 1; x <= 2 * m; x++) {
+                        minOpponentScore = min(minOpponentScore, dp[i + x][max(m, x)]);
+                    }
+                    dp[i][m] = suffixSum[i] - minOpponentScore;
+                }
+            }
+        }
+        
+        return dp[0][1];
     }
 };
